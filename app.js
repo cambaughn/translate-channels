8a8574facf73857eb18e8e816a6913c6601f95d6
Cameron Baughn <cam.baughn@gmail.com>
2023-07-29 19:09:16 -0700

    Add comments to app.js

--- app.js
+++ app.js
@@ -1,48 +1,68 @@
+// Import required libraries
 import { App, ExpressReceiver } from "@slack/bolt";
 import dotenv from 'dotenv';
 dotenv.config();
+
+// Import routes
 import expressRoutes from './routes/expressRoutes.js';
 import slackRoutes from './routes/slackRoutes.js';
+
+// Import teams database
 import teamsDB from "./util/firebaseAPI/teams.js";
 
-console.log('---running app---');
-
+// Create a new Express Receiver
 const expressReceiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+/**
+ * This function is used to authorize the slack app for a specific team.
+ * It fetches the team from the database using the provided team ID, and
+ * throws an error if the team does not have valid access tokens.
+ *
+ * @param {Object} param0 - An object.
+ * @param {string} param0.teamId - The team ID.
+ *
+ * @returns {Object} Returns an object containing botToken, botId, and teamId if authorization is successful.
+ *
+ * @throws {Error} Throws an error if the team does not have valid access tokens.
+ */
 const authorizeFn = async ({ teamId }) => {
   console.log('authorizing for team: ', teamId);
   let team = await teamsDB.getTeam(teamId);
   console.log('team: ', team);
 
+  // If the team has access tokens, return an object containing botToken, botId, and teamId
   if (team.team_access_token && team.bot_user_id && team.id) {
-    // console.log('team has access tokens: ', teamId, team);
     return {
       botToken: team.team_access_token,
       botId: team.bot_user_id,
-      // userToken: user.access_token,
       teamId: team.slack_team_id
     };
   }
 
+  // If the team does not have valid access tokens, throw an error
   throw new Error('No matching authorizations');
 };
 
+// Create a new Slack App with the custom authorize function and express receiver
 const slackApp = new App({
   authorize: authorizeFn,
-  // token: process.env.SLACK_BOT_TOKEN,
-  // signingSecret: process.env.SLACK_SIGNING_SECRET,
   receiver: expressReceiver
 });
 
+// Express App instance
 const expressApp = expressReceiver.app;
 
+// Set up routes for the express and slack apps
 expressRoutes(expressApp, slackApp);
 slackRoutes(slackApp);
 
+// Start the slack app
 (async () => {
   await slackApp.start(process.env.PORT || 3000);
+  
+  // Print a message if the app is running in development mode
   if (process.env.ENVIRONMENT === 'development') {
     console.log('⚡️ Bolt app is running!');
   }

94248baa15e73b2f766a60e5a1d109964ed488c9
Cameron Baughn <cam.baughn@gmail.com>
2023-10-27 23:10:26 -0700

    Update help message to remove support for private channels

--- views/helpMessage.js
+++ views/helpMessage.js
@@ -1,8 +1,9 @@
 const helpMessage = () => {
   let answer = '*FAQs*\n\n\n';
   answer += ":question: *My messages aren't being translated* > Every user must approve the app to translate their messages. This needs to be done *only once*, you can find the button in the *app settings*.\n\n";
-  answer += ':question: *Can I set different settings for different channels* > Yes, translation settings can be added to any public or private channel.\n\n';
-  answer += ':question: *Can I translate messages in private channels or direct messages* > Yes! Private channels are currently supported, and we are exploring supporting DMs as well. Please let us know if this is a key feature for your team.\n\n';
+  answer += ':question: *Can I set different settings for different channels* > Yes, translation settings can be added to any public channel.\n\n';
+  answer += ':question: *Can I translate messages in private channels or direct messages* > Not yet, unfortunately. We are working on private channels right now, and we are exploring supporting DMs as well. Please let us know if this is a key feature for your team.\n\n';
+  // answer += ':question: *Can I translate messages in private channels or direct messages* > Yes! Private channels are currently supported, and we are exploring supporting DMs as well. Please let us know if this is a key feature for your team.\n\n';
   answer += ':question: *Does every message have to be translated* > No, use the command `/nt [YOUR MESSAGE]`and the app will ignore it.\n\n';
   // answer += ':question: *How do I upgrade* > If you are the admin of the workspace, you will see a `Manage Plan` button at the bottom of App Settings. This will take you to your customer portal where you can upgrade/downgrade your plan.\n\n';
   // answer += ':question: *How do I cancel* > Simply delete the app from the workspace and your subscription will be canceled after the current billing period.\n\n';
